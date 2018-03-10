4cbffd6f07fffc4ffe5bef7b68f12a43d3effddf
Shiying Cheng <shiying.s.c@gmail.com>
2018-03-10 16:53:09 -0600

    added map and headline

--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -12,3 +12,7 @@ $(() => {
 });
 
 var chart = require('./_charts.js');
+var L = require("leaflet");
+L.Icon.Default.imagePath = 'https://unpkg.com/leaflet@1.3.1/dist/images/';
+var MiniMap = require('leaflet-minimap');
+var map = require("./_map.js");
